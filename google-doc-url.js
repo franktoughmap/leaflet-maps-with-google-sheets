64463ef874377513c80be5049962f58ec08f9cfc
franktoughmap <35256106+franktoughmap@users.noreply.github.com>
2018-01-11 14:10:33 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,2 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1yjRu24h0sTpcNMuJhMCXh7bcISPUw5-MJpoq4shgNpc/edit#gid=0';
+//var googleDocURL = 'https://docs.google.com/spreadsheets/d/1yjRu24h0sTpcNMuJhMCXh7bcISPUw5-MJpoq4shgNpc/edit#gid=0';
+var googleDocURL = 'file:///C:/franktough_mapdata';
