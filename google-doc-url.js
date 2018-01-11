4d2990c7aa33d21e196d6207bb5f26d9692e3843
franktoughmap <35256106+franktoughmap@users.noreply.github.com>
2018-01-11 15:40:53 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,2 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1yjRu24h0sTpcNMuJhMCXh7bcISPUw5-MJpoq4shgNpc/edit#gid=0';
+//var googleDocURL = 'https://docs.google.com/spreadsheets/d/1yjRu24h0sTpcNMuJhMCXh7bcISPUw5-MJpoq4shgNpc/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
