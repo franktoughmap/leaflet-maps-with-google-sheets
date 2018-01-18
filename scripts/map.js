a2c2707d5f71b4382c1550535a7c7238ff3a3b88
franktoughmap <35256106+franktoughmap@users.noreply.github.com>
2018-01-18 13:19:49 +0100

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -997,7 +997,7 @@ $(window).on('load', function() {
   // Example: 12345678 -> "12,345,678"
    function comma(val) {
       while (/(\d+)(\d{3})/.test(val.toString())) {
-          val = val.toString().replace(/(\d+)(\d{3})/, '$1' + '' + '$2');
+          val = val.toString().replace(/(\d+)(\d{3})/, '$1' + ',' + '$2');
       }
       return val;
   }
